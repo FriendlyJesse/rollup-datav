360ad486bd0b8fc45929e162bccf45ef50354031
Friendlyjesse <jessexinyu@foxmail.com>
2020-11-24 16:59:18 +0800

    update

--- src/components/Test/Test.vue
+++ src/components/Test/Test.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="test">
-    {{message}}
+  <div>
+    <svg width="500" height="200" viewBox="0 0 50 20" style="border: 1px solid #000000">
+      <rect x="20" y="10" width="10" height="5" style="stroke: #000000; fill:none;"/>
+    </svg>
+    <svg width="500" height="200" style="border: 1px solid #000000">
+      <rect x="200" y="100" width="100" height="50" stroke-width="10" style="stroke: #000000; fill:none;"/>
+    </svg>
+    <svg 
+      width="500" 
+      height="200" 
+      viewBox="0 0 200 200" 
+      style="border: 1px solid #000000"
+      preserveAspectRatio="none"
+    >
+      <rect x="100" y="100" width="100" height="50" stroke-width="10" style="stroke: #000000; fill:none;"/>
+    </svg>
   </div>
 </template>
 
@@ -15,9 +29,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.test {
-  color: red
-}
-</style>

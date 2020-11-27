38e331b55c003d72595b0b95c480718d671f741d
Friendlyjesse <jessexinyu@foxmail.com>
2020-11-27 17:08:16 +0800

    feat: vscode config

---
+++ src/components/Container/index.js
@@ -0,0 +1,5 @@
+import Container from './index.vue'
+
+export default function(Vue) {
+  Vue.component(Container.name, Container)
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import Icon from './components/Icon'
 import Loading from './components/Loading'
 import FlyBox from './components/FlyBox'
+import Container from './components/Container'
 
 export default function(Vue) {
   Vue.use(Icon)
   Vue.use(Loading)
   Vue.use(FlyBox)
+  Vue.use(Container)
 }
